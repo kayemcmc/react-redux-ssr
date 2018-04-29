0cd86db18abf221c78606ba5d1c4881a363ae29f
Karen Posada <k100posada@gmail.com>
2018-04-28 19:41:11 -0500

    setting up the client store and the server redux store

--- src/client/client.js
+++ src/client/client.js
@@ -2,10 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import Home from './components/Home';
 import Routes from './Routes';
 
+const store = createStore(reducers, {}, applyMiddleware(thunk))
+
 ReactDOM.hydrate(
-    <BrowserRouter>
-    <Home />
-    </BrowserRouter>, document.querySelector('#root'));
+    <Provider store={store}>
+        <BrowserRouter>
+         <Home />
+        </BrowserRouter>
+    </Provider>, document.querySelector('#root'));
